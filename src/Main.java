d9e0ecfa342847d87c3a6d2193b7f290eb1f049d
Bola KONE <lamsabola@gmail.com>
2024-01-26 15:09:57 +0000

    String function charAt()

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,18 @@ public class Main {
     public static void main(String[] args) {
         int studentAge = 15;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'K';
-        char studentLastInitial = 'H';
         boolean hasPerfectAttendance = true;
+        String studentFirstName = "KONE";
+        String studentLastName = "Lamsa";
+        char studentFirstInitial = studentFirstName.charAt(0);
+        char studentLastInitial = studentLastName.charAt(0);
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
         System.out.println(studentFirstInitial);
         System.out.println(studentLastInitial);
-        System.out.println(hasPerfectAttendance);
     }
 }
